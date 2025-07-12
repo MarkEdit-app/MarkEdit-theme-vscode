99b9e43a82d3464c2465801a41a75fab890e34a4
Ying Zhong <0x00eeee@gmail.com>
2025-07-12 12:24:16 +0800

    Better color scheme control

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 Copy [dist/markedit-theme-vscode.js](dist/markedit-theme-vscode.js) to `~/Library/Containers/app.cyan.markedit/Data/Documents/scripts/`.
 
 You can also run `yarn install && yarn build` to build and deploy the script.
+
+## Settings
+
+In [settings.json](https://github.com/MarkEdit-app/MarkEdit/wiki/Customization#advanced-settings), you can define a settings node named `extension.markeditThemeVSCode` to configure this extension, default settings are:
+
+```json
+{
+  "extension.markeditThemeVSCode": {
+    "enabledMode": "both"
+  }
+}
+```
+
+- `enabledMode`: Enabled color scheme (`both`, `light`, `dark`, or `none` to disable).
--- main.ts
+++ main.ts
@@ -1,19 +1,28 @@
-import { overrideThemes } from 'markedit-theming';
+import { CustomTheme, overrideThemes } from 'markedit-theming';
+import { MarkEdit, type JSONObject, type JSONValue } from 'markedit-api';
 import { vscodeLight, vscodeDark } from '@uiw/codemirror-theme-vscode';
 
+const toObject = (value: JSONValue, fallback = {}) => (value ?? fallback) as JSONObject;
+const rootValue = toObject(toObject(MarkEdit.userSettings)['extension.markeditThemeVSCode']);
+const enabledMode = (rootValue.enabledMode ?? 'both') as string;
+
+const lightTheme: CustomTheme = {
+  extension: vscodeLight,
+  colors: {
+    accentColor: '#0070c1',
+    syntaxMarker: '#a31515',
+  },
+};
+
+const darkTheme: CustomTheme = {
+  extension: vscodeDark,
+  colors: {
+    accentColor: '#9cdcfe',
+    syntaxMarker: '#ce9178',
+  },
+};
+
 overrideThemes({
-  light: {
-    extension: vscodeLight,
-    colors: {
-      accentColor: '#0070c1',
-      syntaxMarker: '#a31515',
-    },
-  },
-  dark: {
-    extension: vscodeDark,
-    colors: {
-      accentColor: '#9cdcfe',
-      syntaxMarker: '#ce9178',
-    },
-  },
+  light: ['both', 'light'].includes(enabledMode) ? lightTheme : undefined,
+  dark: ['both', 'dark'].includes(enabledMode) ? darkTheme : undefined,
 });
