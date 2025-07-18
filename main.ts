ff0e1d658f55be09b2f9f24fcd93453acef2cc55
Ying Zhong <0x00eeee@gmail.com>
2025-07-18 21:06:56 +0800

    Simplify enabledMode with new markedit-theming

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
-import { CustomTheme, overrideThemes } from 'markedit-theming';
-import { MarkEdit, type JSONObject, type JSONValue } from 'markedit-api';
+import { overrideThemes } from 'markedit-theming';
 import { vscodeLight, vscodeDark } from '@uiw/codemirror-theme-vscode';
 
-const toObject = (value: JSONValue, fallback = {}) => (value ?? fallback) as JSONObject;
-const rootValue = toObject(toObject(MarkEdit.userSettings)['extension.markeditThemeVSCode']);
-const enabledMode = (rootValue.enabledMode ?? 'both') as string;
-
-const lightTheme: CustomTheme = { extension: vscodeLight };
-const darkTheme: CustomTheme = { extension: vscodeDark };
-
 overrideThemes({
-  light: ['both', 'light'].includes(enabledMode) ? lightTheme : undefined,
-  dark: ['both', 'dark'].includes(enabledMode) ? darkTheme : undefined,
+  light: { extension: vscodeLight },
+  dark: { extension: vscodeDark },
+  options: { settingsKey: 'extension.markeditThemeVSCode' },
 });
